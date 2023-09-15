5f1f7f518273327a44d5197a0da6903b7e6975cf
deepesh <deepesh@adobe.com>
2023-09-15 09:26:25 -0700

    feat: changes

--- packages/gatsby-theme-aio/src/components/GetCredential/index.js
+++ packages/gatsby-theme-aio/src/components/GetCredential/index.js
@@ -15,88 +15,98 @@ import PropTypes from 'prop-types';
 import { ActionButton } from '../ActionButton';
 // import { Bug, Edit } from '../Icons';
 import { css } from '@emotion/react';
-import JSZip from "jszip";
-import JSZipUtils from 'jszip-utils';
-import saveAs from 'jszip/vendor/FileSaver';
 import {Side} from './side';
 // import { getExternalLinkProps } from '../../utils';
 
-const createCredential = async (setCredentialData, downloads) => {
-  console.log(downloads);
-  // const token = window.adobeIMS?.getTokenFromStorage()?.token;
-  // if (token) {
-  //   const data = {
-  //     name: Date.now().toString(),
-  //     platform: 'apiKey',
-  //     description: 'created for get credential'
-  //   };
-  //   const response = await fetch("/console/api/organizations/918/integrations/adobeid", {
-  //     method: "POST", // or 'PUT'
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Authorization": "Bearer " + token,
-  //       "x-api-key": "UDPWeb1"
-  //     },
-  //     body: JSON.stringify(data),
-  //   });
+const createCredential = async (setCredentialData) => {
+  // console.log(downloads);
+  const token = window.adobeIMS?.getTokenFromStorage()?.token;
+  if (token) {
+    const orgsResponse = await fetch("/console/api/organizations", {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token,
+        "x-api-key": "stage_adobe_io"
+      }
+    });
 
-  //   if (response.status === 200) {
-  //     const result = await response.json();
-  //     const data = await JSZipUtils.getBinaryContent(sampleCodeUrl);
-  //     const zip = await JSZip.loadAsync(data);
-  //     zip.remove("credential.json");
-  //     zip.file("credential.json", JSON.stringify(result, null, " "));
-  //     const blob = await zip.generateAsync({ type: "blob" });
-  //     saveAs(blob, 'sample.zip');
-  //     setCredentialData(result);
-  //   }
-  // }
-  // else {
-  //   console.log('User not logged in');
-  // }
+    if (orgsResponse.status === 200) {
+      console.log(await orgsResponse.json());
+    }
+
+    const data = {
+      name: Date.now().toString(),
+      platform: 'apiKey',
+      description: 'created for get credential'
+    };
+    const response = await fetch("/console/api/organizations/224587/integrations/adobeid", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token,
+        "x-api-key": "stage_adobe_io"
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.status === 200) {
+      const result = await response.json();
+      // const data = await JSZipUtils.getBinaryContent(sampleCodeUrl);
+      // const zip = await JSZip.loadAsync(data);
+      // zip.remove("credential.json");
+      // zip.file("credential.json", JSON.stringify(result, null, " "));
+      // const blob = await zip.generateAsync({ type: "blob" });
+      // saveAs(blob, 'sample.zip');
+      setCredentialData(result);
+    }
+  }
+  else {
+    console.log('User not logged in');
+  }
 }
 
-const GetCredential = ({credentialType, children}) => {
+const GetCredential = () => {
   const [credentialData, setCredentialData] = useState(null);
   let side = "no side component";
-  let content = [];
-  React.Children.forEach(children, (child) => {
-    if (!React.isValidElement(child)) return;
-    console.log(child.type);
-    if (child.type === Side) {
-      console.log('found side component');
-      side = child;
-    } else {
-      content.push(child);
-    }
-  });
+  // let side = "no side component";
+  // let content = [];
+  // React.Children.forEach(children, (child) => {
+  //   if (!React.isValidElement(child)) return;
+  //   console.log(child.type);
+  //   if (child.type === Side) {
+  //     console.log('found side component');
+  //     side = child;
+  //   } else {
+  //     content.push(child);
+  //   }
+  // });
 
-  return side;
-  // if (window.adobeIMS?.isSignedInUser()) {
-  //   return (
-  //     credentialData ? <>
-  //       <div>
-  //         Api Key: {credentialData.apiKey} <br />
-  //         Credential Id: {credentialData.id}
-  //       </div>
-  //       {side ? (side) : (<></>)}
-  //     </> : <div
-  //       css={css`
-  //         display: flex;
-  //       `}>
-  //       <ActionButton
-  //         css={css`
-  //         border-color: var(--spectrum-actionbutton-m-border-color,var(--spectrum-alias-border-color));
-  //         color: var(--spectrum-actionbutton-m-text-color,var(--spectrum-alias-text-color));
-  //         padding: var(--spectrum-global-dimension-size-65)    
-  //         `}
-  //         onClick={() => createCredential(setCredentialData, props)}>
-  //         Get Credential
-  //       </ActionButton>
-  //       {side ? (side) : (<></>)}
-  //     </div>
-  //   );
-  // }
+  // return side;
+  if (window.adobeIMS?.isSignedInUser()) {
+    return (
+      credentialData ? <>
+        <div>
+          Api Key: {credentialData.apiKey} <br />
+          Credential Id: {credentialData.id}
+        </div>
+        {side ? (side) : (<></>)}
+      </> : <div
+        css={css`
+          display: flex;
+        `}>
+        <ActionButton
+          css={css`
+          border-color: var(--spectrum-actionbutton-m-border-color,var(--spectrum-alias-border-color));
+          color: var(--spectrum-actionbutton-m-text-color,var(--spectrum-alias-text-color));
+          padding: var(--spectrum-global-dimension-size-65)    
+          `}
+          onClick={() => createCredential(setCredentialData)}>
+          Get Credential
+        </ActionButton>
+        {side ? (side) : (<></>)}
+      </div>
+    );
+  }
 
   return <></>;
 };
